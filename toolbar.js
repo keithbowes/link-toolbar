781e861cb68a13de7f39f0c2520de7317b236a29
Keith Bowes <keithbowes@users.noreply.github.com>
2025-04-20 23:56:22 -0400

    Make the toolbar hopefully less intrusive, added "author" to the top links instead of in the "Other" dropdown

--- toolbar.js
+++ toolbar.js
@@ -1,8 +1,22 @@
 if (window.hasRun) throw "Already run";
 window.hasRun = true;
 
+var opacity = 1.0;
+var position = 'static';
+var zindex = 1;
+for (var elem of document.querySelectorAll('*')) {
+    var curStyle = document.defaultView.getComputedStyle(elem, null);
+    if ((curStyle.getPropertyValue('position') == 'fixed') && curStyle.getPropertyValue('top') == '0px' && curStyle.getPropertyValue('z-index') != 'auto') {
+        opacity = 0.75;
+        position = 'fixed';
+    }
+    if (curStyle.getPropertyValue('z-index') != 'auto' && curStyle.getPropertyValue('z-index') >= zindex) {
+        zindex = curStyle.getPropertyValue('z-index') + 1;
+    }
+}
+
 var linktoolbar = document.createElement('div');
-linktoolbar.setAttribute('style', 'position: fixed; border: thin black solid; opacity: 0.75; right: 0; z-index: 4000');
+linktoolbar.setAttribute('style', 'position: ' + position + '; border: thin black solid; opacity: ' + opacity + '; right: 0; z-index: ' + zindex);
 document.body.insertBefore(linktoolbar, document.body.firstChild);
 
 try {
@@ -13,19 +27,21 @@ try {
     elem.setAttribute('height', 16);
     elem.setAttribute('width', 16);
     linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
 } catch (e) {
 }
 
 try {
-    var toplink = document.querySelector('link[rel=top]').href;
+    var homelink = document.querySelector('link[rel=home]').href;
 } catch (e) {
-    var toplink = '/';
+    var homelink = '/';
 } finally {
     var tnode = document.createTextNode('Top');
     var elem = document.createElement('a');
-    elem.setAttribute('href', toplink);
+    elem.setAttribute('href', homelink);
     elem.appendChild(tnode);
     linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
 }
 
 try {
@@ -38,6 +54,7 @@ try {
     elem.setAttribute('href', uplink);
     elem.appendChild(tnode);
     linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
 }
 
 try {
@@ -47,6 +64,7 @@ try {
     elem.setAttribute('href', startlink);
     elem.appendChild(tnode);
     linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
 } catch (e) {
 }
 
@@ -57,9 +75,11 @@ try {
     elem.setAttribute('href', firstlink);
     elem.appendChild(tnode);
     linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
 } catch (e) {
 }
 
+/* 'previous' is an alias of 'prev' allowed in HTML 4 and older but only 'prev' is allowed in HTML 5. */
 try {
     var prevlink = document.querySelector('link[rel=prev],link[rel=previous]').href;
     var tnode = document.createTextNode('Prev');
@@ -67,6 +87,7 @@ try {
     elem.setAttribute('href', prevlink);
     elem.appendChild(tnode);
     linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
 } catch (e) {
 }
 
@@ -77,6 +98,7 @@ try {
     elem.setAttribute('href', nextlink);
     elem.appendChild(tnode);
     linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
 } catch (e) {
 }
 
@@ -87,10 +109,28 @@ try {
     elem.setAttribute('href', lastlink);
     elem.appendChild(tnode);
     linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
 } catch (e) {
 }
 
-/* Note: Bookmarks were removed from HTML 5 */
+try {
+    var authorlink = document.querySelector('link[rel=author]').href;
+    var authortitle = document.querySelector('link[rel=author]').title;
+} catch (e) {
+    /* Eh, what Lynx does. It might not be the best thing to do. */
+    var authorlink = 'mailto:webmaster@' + location.hostname;
+    var authortitle = '*Author'; // Use an asterisk to indicate that it might not work.
+} finally {
+    var tnode = document.createTextNode(authortitle || 'Author');
+    var elem = document.createElement('a');
+    elem.setAttribute('href', authorlink);
+    elem.appendChild(tnode);
+    linktoolbar.appendChild(elem);
+    linktoolbar.appendChild(document.createTextNode(' '));
+}
+
+/* Note: rel="bookmark" for <link> was removed from HTML 5 but it still can be
+ * used for <a> and <area> to specify a permalink. */
 var bookmarks = document.querySelectorAll('link[rel=bookmark]');
 if (bookmarks.length > 0) {
     var bookmarks_combo = document.createElement('select');
@@ -101,6 +141,7 @@ if (bookmarks.length > 0) {
     elem.appendChild(tnode);
     bookmarks_combo.appendChild(elem);
     linktoolbar.appendChild(bookmarks_combo);
+    linktoolbar.appendChild(document.createTextNode(' '));
 }
 for (bookmark of bookmarks) {
     var tnode = document.createTextNode(bookmark.title || bookmark.rel);
@@ -121,9 +162,13 @@ if (alternates.length > 0) {
     elem.appendChild(tnode);
     alternates_combo.appendChild(elem);
     linktoolbar.appendChild(alternates_combo);
+    linktoolbar.appendChild(document.createTextNode(' '));
 }
 for (var alternate of alternates) {
-    var tnode = document.createTextNode(alternate.title || alternate.rel);
+    var title = alternate.title || alternet.rel;
+    if (alternate.hreflang)
+        title += ' [' + alternate.hreflang + ']';
+    var tnode = document.createTextNode(title);
     var elem  = document.createElement('option');
     elem.setAttribute('title', alternate.href);
     elem.setAttribute('value', alternate.href);
@@ -139,6 +184,7 @@ if (tstyles.length > 1) {
     styles_group.setAttribute('label', 'Available Stylesheets');
     styles_combo.appendChild(styles_group);
     linktoolbar.appendChild(styles_combo);
+    linktoolbar.appendChild(document.createTextNode(' '));
 
     for (var style of tstyles) {
         var tnode = document.createTextNode(style.title);
@@ -154,13 +200,13 @@ if (tstyles.length > 1) {
 
 var linksel = new Array();;
 /* Any valid rel types not previously handled */
-var rels=['author', 'help'];
-rels = rels.concat(['contents', 'index', 'glossary', 'copyright', 'chapter', 'section', 'subsection', 'appendix']); // HTML 4 only
-rels = rels.concat(['icon', 'license', 'search']); // HTML 5 only; there's also 'dns-prefetch', 'pingback', 'preconnect', 'prefetch', 'preload', and 'prerender', but those aren't interesting to a user but only to a browser or other user agent.
+var rels=['help'];
+rels = rels.concat(['contents', 'index', 'glossary', 'copyright', 'chapter', 'section', 'subsection', 'appendix']); // Removed in HTML 5
+rels = rels.concat(['license', 'search']); // Added in HTML 5; there's also 'dns-prefetch', 'icon', 'pingback', 'preconnect', 'prefetch', 'preload', and 'prerender', but those aren't interesting to a user but only to a browser or other user agent.
 for (var rel of rels) {
     linksel.push('link[rel~=' + rel + ']');
 }
-linktoolbar.appendChild(document.createTextNode(linksel.join(', ')));
+otherlinks = document.querySelectorAll(linksel.join(', '));
 if (otherlinks.length > 0) {
     var other_combo = document.createElement('select');
     other_combo.setAttribute('onchange', "location.href=this.options[this.selectedIndex].value; this.options[0].disabled=true");
